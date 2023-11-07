082c3f785488959e8f3e6901b4329c7b7cb4720c
lexiama <alexia.mckoy@gmail.com>
2023-11-06 19:18:18 -0500

    update

---
+++ scripts/questions.js
@@ -0,0 +1,9 @@
+//const filteredActivities = activities.filter(function(activity) {
+   // return activity.category === category; 
+
+
+   // does putting certain scripts above the others have an effect on the code? --- line 26
+
+   //catergoriesSelect.addEventListener("change", function(event)  ----  line 14
+
+   // can we use arrow function for instead of the regular funtctions 
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
+let activityPrice= 0;
+let activityName = "";
+
+
+
+
 // populate catergories for the select menu
 function populateCategories() {
   for (var i = 0; i < categories.length; i++) {
     var option = document.createElement("option");
     option.value = categories[i];
     option.innerText = categories[i];
-    document.getElementById("categoryDropdown").appendChild(option);
+    document.getElementById("categoryDropdown").appendChild(option); // like push 
   }
 }
 
@@ -32,3 +38,51 @@ function populateActivities(category) {
         document.getElementById("activityDropdown").appendChild(option);
       }
 }
+
+
+
+const activitiesSelect = document.getElementById("activityDropdown"); 
+activitiesSelect.addEventListener("change", function(event) {
+  const activity = event.target.value; 
+  populateActivityDetails(activity); 
+});
+
+function populateActivityDetails(name) { 
+  const filteredActivities = activities.filter(function(activity) {
+    return activity.name === name; 
+  });
+   
+    document.getElementById("activityId").innerText = filteredActivities[0].id;
+    document.getElementById("activityName").innerText = filteredActivities[0].name;
+    document.getElementById("activityDescription").innerText = filteredActivities[0].description;
+    document.getElementById("activityLocation").innerText = filteredActivities[0].location;
+    document.getElementById("activityPrice").innerText = filteredActivities[0].price;
+
+    activityPrice = filteredActivities[0].price;
+    activityName = filteredActivities[0].name;
+
+  if (filteredActivities[0].price > 0) {
+        document.getElementById ("ticketPurchaseForm").style.display = "block"; 
+  }
+  else {
+    document.getElementById ("ticketPurchaseForm").style.display = "none"; 
+  }
+}
+
+// purchase tickets
+
+const form = document.getElementById("ticketPurchaseForm");
+form.addEventListener("submit", function(event) { 
+event.preventDefault(); 
+const numberOfTickets = document.getElementById("ticketQuantity").value;
+const email = document.getElementById("email").value;
+const amount = numberOfTickets * activityPrice;
+const confirmationMessage = document.getElementById("purchaseMessage");
+const message = `Your credit card has been charged $${amount} for ${numberOfTickets} tickets to ${activityName}. A confirmation email has been sent to ${email}.`;
+
+confirmationMessage.innerText = message;
+
+//"Your credit card has been charged $(amount) for (number-of- tickets) to
+//(adventure-name). A confirmation email has been sent to (email)."
+
+});
